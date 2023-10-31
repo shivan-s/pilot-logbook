9432c16324d47220c7db494faaba33241fdb401b
Shivan Sivakumaran <me@shivan.xyz>
2023-10-31 22:57:44 +1300

    wip

--- main.go
+++ main.go
@@ -1,13 +1,40 @@
+// Create a page to show logbook with dummy data
+
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"log"
 	"net/http"
+	"os"
 )
 
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".html"
+	body, err := os.ReadFile("templates/" + filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	t, _ := template.ParseFiles("templates/" + tmpl + ".html")
+	t.Execute(w, p)
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Test")
+	title := "Home"
+	p, err := loadPage("index")
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	renderTemplate(w, "index", p)
 }
 
 func main() {
